0bd836c97acc561df6ad7684fb57dcd8545d6250
Arnaud Roques <plantuml@gmail.com>
2025-02-11 23:31:38 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+
+plugins {
+    java
+    id("eclipse")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
